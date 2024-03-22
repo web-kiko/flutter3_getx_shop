cb212aabe59840c639711244f4ede853d90a7c9e
web-kiko <kikoiiii@163.com>
2024-03-23 02:20:51 +0800

    详情页的tab切换和使用SingleChildScrollView实现锚点效果

--- lib/app/modules/productContent/controllers/product_content_controller.dart
+++ lib/app/modules/productContent/controllers/product_content_controller.dart
@@ -2,12 +2,28 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 class ProductContentController extends GetxController {
-
   final ScrollController scrollController = ScrollController();
+  //导航的透明度
   RxDouble opcity = 0.0.obs;
+  //是否显示tabs
+  RxBool showTabs = false.obs;
+  List tabsList = [
+    {
+      "id": 1,
+      "title": "商品",
+    },
+    {"id": 2, "title": "详情"},
+    {"id": 3, "title": "推荐"}
+  ];
+  RxInt selectedTabsIndex = 1.obs;
+  GlobalKey gk1 = GlobalKey();
+  GlobalKey gk2 = GlobalKey();
+  GlobalKey gk3 = GlobalKey();
+
   @override
   void onInit() {
     super.onInit();
+    scrollControllerListener();
   }
 
   @override
@@ -15,13 +31,20 @@ class ProductContentController extends GetxController {
     super.onReady();
   }
 
-//监听滚动条滚动事件
+  //监听滚动条滚动事件
   void scrollControllerListener() {
     scrollController.addListener(() {
       if (scrollController.position.pixels <= 100) {
         opcity.value = scrollController.position.pixels / 100;
-        print(opcity.value);
+        if (showTabs.value == true) {
+          showTabs.value = false;
+        }
         update();
+      } else {
+        if (showTabs.value == false) {
+          showTabs.value = true;
+          update();
+        }
       }
     });
   }
@@ -29,5 +52,10 @@ class ProductContentController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  void changeSelectedTabsIndex(index) {
+    selectedTabsIndex.value = index;
+    update();
   }
 }
--- lib/app/modules/productContent/views/product_content_view.dart
+++ lib/app/modules/productContent/views/product_content_view.dart
@@ -10,205 +10,174 @@ class ProductContentView extends GetView<ProductContentController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-           extendBodyBehindAppBar: true, //实现透明导航
-      appBar:PreferredSize(
-        preferredSize: Size.fromHeight(ScreenAdapter.height(96)),
-        child: Obx(() => AppBar(
-        leading: Container(
-          alignment: Alignment.center,
-          child: SizedBox(
-              width: ScreenAdapter.width(88),
-              height: ScreenAdapter.width(88),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    //注意:去掉button默认的padding
-                    padding: MaterialStateProperty.all(const EdgeInsets.all(0)),
-                    alignment: Alignment.center,
-                    backgroundColor: MaterialStateProperty.all(Colors.black12),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    shape: MaterialStateProperty.all(const CircleBorder())),
-                child: const Icon(
-                  Icons.arrow_back_ios_new_outlined,
+      extendBodyBehindAppBar: true, //实现透明导航
+      appBar: PreferredSize(//圆形
+          preferredSize: Size.fromHeight(ScreenAdapter.height(120)),
+          child: Obx(
+            () => AppBar(
+              leading: Container(
+                alignment: Alignment.center,
+                child: SizedBox(
+                    width: ScreenAdapter.width(88),
+                    height: ScreenAdapter.width(88),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          //注意:去掉button默认的padding
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(0)),
+                          alignment: Alignment.center,
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black12),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shape:
+                              MaterialStateProperty.all(const CircleBorder())),
+                      child: const Icon(
+                        Icons.arrow_back_ios_new_outlined,
+                      ),
+                    )),
+              ),
+              title: SizedBox(
+                width: ScreenAdapter.width(400),
+                height: ScreenAdapter.height(96),
+                child: controller.showTabs.value
+                    ? Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: controller.tabsList.map((v) {
+                          return InkWell(
+                              onTap: () {
+                                controller.changeSelectedTabsIndex(v["id"]);
+                                //跳转到指定的容器
+                                if (v["id"] == 1) {
+                                  Scrollable.ensureVisible(//结合下面的SingleChildScrollView实现锚点跳转
+                                      controller.gk1.currentContext
+                                          as BuildContext,
+                                     
+                                      duration: //动画效果
+                                          const Duration(milliseconds: 100));
+                                } else if (v["id"] == 2) {
+                                  Scrollable.ensureVisible(
+                                      controller.gk2.currentContext
+                                          as BuildContext,
+                                      duration:
+                                          const Duration(milliseconds: 100));
+                                } else {
+                                  Scrollable.ensureVisible(
+                                      controller.gk3.currentContext
+                                          as BuildContext,
+                                      duration:
+                                          const Duration(milliseconds: 500));
+                                }
+                              },
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "${v["title"]}",
+                                    style: TextStyle(
+                                        fontSize: ScreenAdapter.fontSize(36)),
+                                  ),
+                                  v["id"] == controller.selectedTabsIndex.value
+                                      ? Container(
+                                          margin: EdgeInsets.only(
+                                              top: ScreenAdapter.height(10)),
+                                          width: ScreenAdapter.width(100),
+                                          height: ScreenAdapter.height(2),
+                                          color: Colors.red,
+                                        )
+                                      : Container(
+                                          margin: EdgeInsets.only(
+                                              top: ScreenAdapter.height(10)),
+                                          width: ScreenAdapter.width(100),
+                                          height: ScreenAdapter.height(2),
+                                        )
+                                ],
+                              ));
+                        }).toList())
+                    : const Text(""),
+              ),
+              centerTitle: true,
+              backgroundColor:
+                  Colors.white.withOpacity(controller.opcity.value), //实现透明导航
+              elevation: 0, //实现透明导航
+              actions: [
+                Container(
+                  margin: EdgeInsets.only(right: ScreenAdapter.width(20)),
+                  width: ScreenAdapter.width(88),
+                  height: ScreenAdapter.width(88),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          //注意:去掉button默认的padding
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(0)),
+                          alignment: Alignment.center,
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black12),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shape:
+                              MaterialStateProperty.all(const CircleBorder())),
+                      child: const Icon(
+                        Icons.file_upload_outlined,
+                      )),
                 ),
-              )),
-        ),
-        title: SizedBox(
-          width: ScreenAdapter.width(400),
-          height: ScreenAdapter.height(96),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                  padding:const  EdgeInsets.all(0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "商品",
-                        style: TextStyle(fontSize: ScreenAdapter.fontSize(36)),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
-                        width: ScreenAdapter.width(100),
-                        height: ScreenAdapter.height(2),
-                        color: Colors.red,
-                      )
-                    ],
-                  )),   
-                 Padding(
-                  padding: EdgeInsets.all(0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "详情",
-                        style: TextStyle(fontSize: ScreenAdapter.fontSize(36)),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
-                        width: ScreenAdapter.width(100),
-                        height: ScreenAdapter.height(2),
-                        color: Colors.red,
-                      )
-                    ],
-                  )),
-                   Padding(
-                  padding: EdgeInsets.all(0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "推荐",
-                        style: TextStyle(fontSize: ScreenAdapter.fontSize(36)),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
-                        width: ScreenAdapter.width(100),
-                        height: ScreenAdapter.height(2),
-                        color: Colors.red,
-                      )
-                    ],
-                  )),              
-            ],
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white.withOpacity(controller.opcity.value), //实现透明导航
-        elevation: 0, //实现透明导航
-        actions: [
-          Container(
-            margin: EdgeInsets.only(right: ScreenAdapter.width(20)),
-            width: ScreenAdapter.width(88),
-            height: ScreenAdapter.width(88),
-            child: ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    //注意:去掉button默认的padding
-                    padding: MaterialStateProperty.all(const EdgeInsets.all(0)),
-                    alignment: Alignment.center,
-                    backgroundColor: MaterialStateProperty.all(Colors.black12),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    shape: MaterialStateProperty.all(const CircleBorder())),
-                child: const Icon(
-                  Icons.file_upload_outlined,
-                )),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: ScreenAdapter.width(20)),
-            width: ScreenAdapter.width(88),
-            height: ScreenAdapter.width(88),
-            child: ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    //注意
-                    padding: MaterialStateProperty.all(EdgeInsets.all(0)),
-                    alignment: Alignment.center,
-                    backgroundColor: MaterialStateProperty.all(Colors.black12),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    shape: MaterialStateProperty.all(const CircleBorder())),
-                child: const Icon(
-                  Icons.more_horiz_rounded,
-                )),
-          ),
-        ],
-      ),
-      )
-      
-      ) ,
-      body: ListView(
+                Container(
+                  margin: EdgeInsets.only(right: ScreenAdapter.width(20)),
+                  width: ScreenAdapter.width(88),
+                  height: ScreenAdapter.width(88),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          //注意
+                          padding: MaterialStateProperty.all(EdgeInsets.all(0)),
+                          alignment: Alignment.center,
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black12),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shape:
+                              MaterialStateProperty.all(const CircleBorder())),
+                      child: const Icon(
+                        Icons.more_horiz_rounded,
+                      )),
+                ),
+              ],
+            ),
+          )),
+      //singlechildScerollView锚点 controller.gk1.currentContext as BuildContext实现tab跳转   
+      body: SingleChildScrollView(
         controller: controller.scrollController,
-        children: [
-          Container(
-            width: ScreenAdapter.width(1080),
-            height: ScreenAdapter.height(300),
-            color: Colors.orange,
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
+        child: Column(
+          children: [
             Container(
-            width: ScreenAdapter.width(1080),
-            height: ScreenAdapter.height(300),
-            color: Colors.orange,
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
+              key: controller.gk1,
+              alignment: Alignment.center,
+              width: ScreenAdapter.width(1080),
+              height: ScreenAdapter.height(1800),
+              color: Colors.orange,
+              child: const Text("商品", style: TextStyle(fontSize: 100)),
+            ),
             Container(
-            width: ScreenAdapter.width(1080),
-            height: ScreenAdapter.height(300),
-            color: Colors.red,
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-           Container(
-            width: ScreenAdapter.width(1080),
-            height: ScreenAdapter.height(300),
-            color: Colors.blue,
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-          ListTile(
-            title: Text("我是一个内容列表"),
-          ),
-        ],
+              key: controller.gk2,
+              alignment: Alignment.center,
+              width: ScreenAdapter.width(1080),
+              height: ScreenAdapter.height(2900),
+              color: Colors.blue,
+              child: const Text("详情", style: TextStyle(fontSize: 100)),
+            ),
+            Container(
+              key: controller.gk3,
+              alignment: Alignment.center,
+              width: ScreenAdapter.width(1080),
+              height: ScreenAdapter.height(2200),
+              color: Colors.red,
+              child: const Text("推荐", style: TextStyle(fontSize: 100)),
+            ),
+          ],
+        ),
       ),
     );
   }
